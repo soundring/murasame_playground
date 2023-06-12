6e246a0664af38f8c3e8f50a8a5a3e075b10978b
murasame <murasame.usa@gmail.com>
2023-06-13 00:33:11 +0900

    :wrench:サイズが可変になるように改修、WebViewを使うコードを記述
    現在アプリとwebで共存できない

--- lib/model_viewer_app/pages/home_page.dart
+++ lib/model_viewer_app/pages/home_page.dart
@@ -16,44 +16,63 @@ class ModelViewerHomePage extends StatelessWidget {
     final screenHeight = MediaQuery.of(context).size.height;
     final isTablet = screenWidth > 600;
 
-    if (kIsWeb) {
-      // ignore: undefined_prefixed_name
-      ui.platformViewRegistry.registerViewFactory(
-        _viewId,
-        (int viewId) => html.IFrameElement()
-          ..style.width = '${screenWidth}px'
-          ..style.height = '${screenHeight}px'
-          ..src =
-              'https://sketchfab.com/models/9120703a4aee4c2cb0313a9ca3e1e1a3/embed'
-          ..style.border = 'none',
-      );
-
-      return Scaffold(
-        appBar: AppBar(
-            title: const Text('3Dモデルビュワー'),
-            foregroundColor: Colors.white,
-            backgroundColor: const Color(0xFFFF7939)),
-        body: Center(
-          child: SizedBox(
-            width: isTablet ? screenWidth * 0.5 : screenWidth,
-            child: const HtmlElementView(
-              viewType: _viewId,
-            ),
-          ),
-        ),
-      );
-    }
-
-    final WebViewController controller = WebViewController();
-    controller.loadRequest(Uri.parse(
-        'https://sketchfab.com/models/9120703a4aee4c2cb0313a9ca3e1e1a3/embed'));
+    // ignore: undefined_prefixed_name
+    ui.platformViewRegistry.registerViewFactory(
+      _viewId,
+      (int viewId) => html.IFrameElement()
+        ..style.width = '100%'
+        ..style.height = '100%'
+        ..src =
+            'https://sketchfab.com/models/9120703a4aee4c2cb0313a9ca3e1e1a3/embed'
+        ..style.border = 'none',
+    );
 
     return Scaffold(
       appBar: AppBar(
           title: const Text('3Dモデルビュワー'),
           foregroundColor: Colors.white,
           backgroundColor: const Color(0xFFFF7939)),
-      body: WebViewWidget(controller: controller),
+      body: Column(
+        children: [
+          const Text('3Dモデルを表示します。'),
+          Expanded(
+            child: SizedBox(
+              width: double.infinity,
+              height: double.infinity,
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  maxWidth: isTablet ? screenWidth * 0.5 : screenWidth,
+                  maxHeight: screenHeight * 0.5,
+                ),
+                child: const HtmlElementView(
+                  viewType: _viewId,
+                  key: Key(_viewId),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
+
+    // final WebViewController controller = WebViewController();
+    // controller
+    //   ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    //   ..setBackgroundColor(Colors.white)
+    //   ..loadRequest(Uri.parse(
+    //       'https://sketchfab.com/models/9120703a4aee4c2cb0313a9ca3e1e1a3/embed'));
+    //
+    // return Scaffold(
+    //   appBar: AppBar(
+    //       title: const Text('3Dモデルビュワー'),
+    //       foregroundColor: Colors.white,
+    //       backgroundColor: const Color(0xFFFF7939)),
+    //   body: Center(
+    //     child: SizedBox(
+    //       width: isTablet ? screenWidth * 0.5 : screenWidth,
+    //       child: WebViewWidget(controller: controller),
+    //     ),
+    //   ),
+    // );
   }
 }
